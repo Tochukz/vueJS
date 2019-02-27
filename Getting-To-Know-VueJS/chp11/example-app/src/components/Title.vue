a37e87176ebf5e36d98243739ba380cb038b9d9d
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-02-27 20:41:26 +0200

    Single File Components (SFC) and toolings

---
+++ Getting-To-Know-VueJS/chp11/example-app/src/components/Title.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Title: {{ title }}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: "First Single File Component"
+        }
+    }
+}
+</script>
+
+<style scoped>
+ h1 {
+     color: blue;
+ }
+</style>
