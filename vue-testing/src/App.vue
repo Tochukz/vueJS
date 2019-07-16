ae817f2b1d064360b7e59e62a2ac780c556f453b
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-07-16 22:45:49 +0200

    using the eventBus

---
+++ vue-testing/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+     <sheep />
+     <goat />
+     
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue';
+import Goat from './components/Goat';
+import Sheep from './components/Sheep';
+
+export default {
+  name: 'app',
+  components: {
+    Goat,
+    Sheep
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-testing/src/components/Sheep.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h2>Sheep Component</h2>
+      <p v-text="sheep"></p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    sheep: '...'
+  }),
+  methods: {
+    addMessage(sheepName) {
+      this.sheep = sheepName;
+    }
+  },
+  created() {    
+    this.eventBus.$on('transfer-sheep', this.addMessage); 
+  },
+  inject: ['eventBus'],
+}
+</script>
