0fe78217875c552b197c2ff30db5c405c4e7e222
Tochukwu Nwachukwu <truetochukz@gmail.com>
2020-01-05 10:17:38 +0200

    SportsStore: Checkout and Orders

--- ProVueJS/chp6/sportstore/src/store/orders.js
+++ ProVueJS/chp6/sportstore/src/store/orders.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const ORDERS_URL = "http://localhost:3500/orders";
 
-export default {    
+export default {
     actions: {
         async storeOrder(context, order) {
             order.cartLines = context.rootState.cart.lines;
@@ -12,9 +12,9 @@ export default {
 }
 
 /**
- * The rootState property of the context object is used to navigate to other store module's state 
- * 
- * The namespace feature is not enabled in this module, which means that it's getters, mutations and actions 
- * will not be merged with those iin the index.js file and will not be accessed using a prefix.
- * However, the state properties are always prefixed event when the nameapsce feature is not used.
+ * The rootState property of the context object is used to navigate to other store module's state
+ *
+ * The namespace feature is not enabled in this module, which means that it's getters, mutations and actions
+ * will not be merged with those in the index.js file and will not be accessed using a prefix.
+ * However, the state properties are always prefixed even when the nameapsce feature is not used.
  */
